afbb04a8af0633865bfda41c569e8b75d8cd8b21
Tri Tran <tritran@Tris-MacBook-Air-3.local>
2025-02-21 11:14:17 -0500

    stub for system report

---
+++ script02/system_report.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+###
+#   Tri Tran
+#   NSSA-221.03
+#   Friday, February 21, 2024
+###
+
+import os
+import platform
+
+tabs = "\t\t\t\t\t" # Define standard tabbing across entire report
+
+# Identify hostname and domain
+def device_info():
+    print("Device Information")
+
+
+# Identify IP address, gateway, network mask, primary/secondary DNS
+def network_info():
+    print("TODO")
+
+# Identify operating system/version and kernel version
+def os_info():
+    print("TODO")
+
+# Identify total drive storage, storage used/free
+def storage_info():
+    print("TODO")
+
+# Identify CPU model, number of processors/cores
+def processor_info():
+    print("TODO")
+
+# Identify total and available RAM
+def mem_info():
+    print("TODO")
+    
+def main():
+    print("System Report - ", end="")
+    os.system("date")
+    print("\n")
+
+    device_info()
+    network_info()
+    os_info()
+    storage_info()
+    processor_info()
+    mem_info()
