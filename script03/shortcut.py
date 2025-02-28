b3e11ce446c761466057506706f713df9028bfbd
Tri Tran <tritran@Tris-MacBook-Air-3.local>
2025-02-28 11:23:58 -0500

    link report stub

---
+++ script03/shortcut.py
@@ -0,0 +1,21 @@
+#!bin/python3.9
+
+import os
+
+def create_link():
+    print("TODO")
+
+def delete_link():
+    print("TODO")
+
+def link_report():
+    print("TODO")
+
+def main():
+    os.system("clear")
+
+    create_link()
+    delete_link()
+    link_report()
+
+main()
